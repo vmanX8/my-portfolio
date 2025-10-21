d92329e1dbfd26b720f4116cc50fe2ef44b736d3
vmanX8 <v.manouhos@gmail.com>
2025-10-21 10:28:27 +0300

    fix issues

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/base.css';
 import './styles/layout.css';
-import './styles/components.css';
+import './styles/compnts.css';
 import './styles/sections.css';
 
 createRoot(document.getElementById('root')!).render(
